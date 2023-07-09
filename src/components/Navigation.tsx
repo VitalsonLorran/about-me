4eca5be77d9e0334a7d2e900889d3e319f7f41e5
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-09 18:28:43 -0300

    Iniciando a página

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,8 @@
+export const Navigation = () => {
+    return(
+        <div className="flex justify-around items-center  h-16 w-full bg-gray-800 fixed">
+            <div className="flex h-full w-full items-center justify-center cursor-pointer bg-slate-400">Logo</div>
+            <div className="flex h-full w-52 items-center justify-center bg-red-300 cursor-pointer">Navigation</div>
+        </div>
+    )
+}
