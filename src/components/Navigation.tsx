c54dd6ab5dc5c8929d31e37d8166e6743e55d447
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-10 09:44:57 -0300

    Atualizando umas coisinhas

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,14 @@
 export const Navigation = () => {
     return(
-        <div className="flex justify-start  items-start h-16 w-full bg-gray-800 fixed">
-            <div className="flex h-full w-full items-center justify-center cursor-pointer bg-slate-400">Logo</div>
-            <div className="flex h-full w-52 items-center justify-center bg-red-300 cursor-pointer">Navigation</div>
+        <div className="flex justify-start  items-start h-14 w-full  fixed">
+            <div className="flex h-full w-full items-center justify-center bg-slate-400">
+                <div className="cursor-pointer ">Logo</div>
+            </div>
+            <div className="flex flex-col h-full w-48 items-center justify-center cursor-pointer">
+                <div className="w-10 border border-white my-1"> </div>
+                <div className="w-10 border border-white my-1"> </div>
+                <div className="w-10 border border-white my-1"> </div>
+            </div>
         </div>
     )
 }
