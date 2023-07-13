eafa1febf308184d56bccd4405eb98b6e5e3192e
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-13 20:53:00 -0300

    Hover:Black
    Adicionando algumas coisas visuais com Hover, que muda a cor dos botões e das redes sociais.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,23 @@ import { CiLinkedin } from "react-icons/ci"
 import { BsTwitter } from "react-icons/bs"
 import { ImInstagram } from "react-icons/im"
 export const Footer = () => {
+    const css: string = "h-10 w-10 mx-3 hover:fill-black hover:mb-4 hover:transition hover:duration-500"
+
     return (
         <div className='flex justify-center items-center w-full h-20 absolute bottom-0 left-0  '> {/**posicionando ele em baixo: absolute bottom e left:0 */}
-            <a target="_blank" href="https://twitter.com/vitalsonlorran"><BsTwitter className="h-10 w-10 mx-3"/> </a>
-            <a target="_blank" href="https://github.com/VitalsonLorran"><AiFillGithub className="w-10 h-10 mx-3" /></a>
-            <a target="_blank" href="https://www.linkedin.com/in/vitalsonlorran/"><CiLinkedin  className="w-10 h-10 mx-3"/></a>
-            <a target="_blank" href="https://www.instagram.com/vitalsonlorran"><ImInstagram className="w-10 h-10 mx-3" /></a>
+            <a target="_blank" href="https://twitter.com/vitalsonlorran">
+                <BsTwitter 
+                    className= {css}/> 
+            </a>
+            <a target="_blank" href="https://github.com/VitalsonLorran">
+                <AiFillGithub className= {css} />
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/vitalsonlorran/">
+                <CiLinkedin  className= {css}/>
+            </a>
+            <a target="_blank" href="https://www.instagram.com/vitalsonlorran">
+                <ImInstagram className= {css} />
+            </a>
         </div>
     )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,21 +3,22 @@ export const Navigation = () => {
     const clickNav = () => {
 
     }
-    
+    const cssHead:string = 'cursor-pointer hover:text-black hover:underline hover:pb-4 hover:transition hover:duration-500'
+    const cssNav:string = 'w-8 border border-white my-1'
     return(
         <div className="flex justify-start  items-start h-14 w-full mb-14 fixed">
             <div className="flex ml-20 h-full w-full items-center justify-around  text-2xl">
-                <div className="cursor-pointer hover:text-black hover:underline"><a href="">Home</a></div>
-                <div className="cursor-pointer hover:text-black hover:underline"><a href="">Projetos</a></div>
-                <div className="cursor-pointer hover:text-black hover:underline"><a href="">About-me</a></div>
-                <div className="cursor-pointer hover:text-black hover:underline"><a href="">Contato</a></div>
+                <div className={cssHead}><a href="">Home</a></div>
+                <div className={cssHead}><a href="">Projetos</a></div>
+                <div className={cssHead}><a href="">About-me</a></div>
+                <div className={cssHead}><a href="">Contato</a></div>
             </div>
             <button 
             onClick={clickNav}
             className="flex flex-col h-full w-48 items-center justify-center cursor-pointer">
-                <div className="w-8 border border-white my-1"> </div>
-                <div className="w-8 border border-white my-1"> </div>
-                <div className="w-8 border border-white my-1"> </div>
+                <div className={ cssNav }> </div>
+                <div className={ cssNav }> </div>
+                <div className={ cssNav }> </div>
             </button>
         </div>
     )
