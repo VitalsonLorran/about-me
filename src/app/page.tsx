5f9db004e987c40cb2412450176a25c3119b8aee
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-20 19:13:06 -0300

    Rotas com problema mas...
    Preciso que quando clique em um botao, renderize o que foi clicado no meio da tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@
 import { BoxRotate } from "@/components/BoxRotate";
 import { Footer } from "@/components/Footer";
 import { Navigation } from "@/components/Navigation";
-import Aboutme from "./pages/Aboutme";
-import Contato from "./pages/Contato";
-import Projetos from "./pages/Projetos";
+import Contato from "../../pages/api/Contato";
 
 
 export default function Home() {
@@ -14,9 +12,10 @@ export default function Home() {
     <div className="bg-gray-600 h-screen w-screen">
       
       <Navigation />
-      
-      
       <div className="h-14"></div>
+      <div className="max-w-screen-xl m-auto bg-black">
+        <Contato/>
+      </div>
       <BoxRotate color={"bg-black"}
       className ="absolute bottom-0 mb-10 ml-10"
       />
