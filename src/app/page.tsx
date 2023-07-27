1a9c5b58877ed7be3118ebd9445c4f44a5ee4885
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-26 22:36:40 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <TopComponents />
       <div className="max-w-screen-xl m-auto bg-black text-center p-10">
         
-        {window.location.pathname == "/" ? "Início": ""}
+        {window.location.pathname == "/" ? "Início 1": ""}
       </div>
       <BottomComponents />
     </div>
